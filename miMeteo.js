dbdecee9d8585d9ab56b7cc735445e956a444933
anass taddart <anasbaclibr@gmail.com>
2025-01-26 17:38:14 +0100

    Add event  to search btn

--- miMeteo.js
+++ miMeteo.js
@@ -4,3 +4,16 @@ const searchButton = document.getElementById("searchButton");
 const cityName = document.getElementById("cityName");
 const tempValue = document.getElementById("tempValue");
 const weatherIcon = document.getElementById("weatherIcon");
+
+ // Add event  to search btn 
+searchButton.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent form from refreshing the page
+    
+    const city = searchInput.value; // the city entered by the user
+    if (city) {
+      fetchWeatherData(city); // Call the function to fetch weather data
+    } else {
+      alert("Please enter a city name!");
+    }
+  });
+  
