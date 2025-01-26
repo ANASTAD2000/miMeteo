816a694dd83f48aff4eec90ae75b10b51f6ce0b6
anass taddart <anasbaclibr@gmail.com>
2025-01-26 17:21:46 +0100

    start in js file (DOM elements)

---
+++ miMeteo.js
@@ -0,0 +1,6 @@
+// DOM Elements 
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const cityName = document.getElementById("cityName");
+const tempValue = document.getElementById("tempValue");
+const weatherIcon = document.getElementById("weatherIcon");
